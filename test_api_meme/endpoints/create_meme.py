0ff0bb44fc8f543a7ecbfbdd37376ddbf24d6c28
AndrewShabailov <andrewshabailov@gmail.com>
2026-05-29 16:03:08 +0300

    linter fix

--- test_api_meme/endpoints/create_meme.py
+++ test_api_meme/endpoints/create_meme.py
@@ -1,7 +1,7 @@
 import requests
 import allure
-from data.payloads import payload
 from endpoints.base_meme import BaseMeme
+
 
 class CreateMeme(BaseMeme):
 
@@ -10,5 +10,9 @@ class CreateMeme(BaseMeme):
         self.response = requests.post(
             f'{self.base_url}/meme', json=payload, headers=self.headers
         )
-        allure.attach(self.response.text, name="Response Body", attachment_type=allure.attachment_type.JSON)
+        allure.attach(
+            self.response.text,
+            name="Response Body",
+            attachment_type=allure.attachment_type.JSON
+        )
         return self.response
