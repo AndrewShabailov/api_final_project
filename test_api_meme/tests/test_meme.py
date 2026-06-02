345c543762401015d3e2dda1d3b19aedf52f1ced
AndrewShabailov <andrewshabailov@gmail.com>
2026-06-02 15:52:48 +0300

    added token is alive and update tests

--- test_api_meme/tests/test_meme.py
+++ test_api_meme/tests/test_meme.py
@@ -1,5 +1,6 @@
 import pytest
 from data.payloads import payload
+from data.payloads import upd_payload
 
 
 @pytest.mark.positive
@@ -9,9 +10,28 @@ def test_get_all_memes(start_end_testing, before_after_testing, read_meme_endpoi
 
 
 @pytest.mark.positive
+def test_token_is_alive(start_end_testing, before_after_testing, read_meme_endpoint):
+    read_meme_endpoint.read_meme()
+    read_meme_endpoint.read_token_is_alive()
+    read_meme_endpoint.check_status_code_is_200()
+
+
+@pytest.mark.positive
 def test_create_meme(start_end_testing, before_after_testing, meme_factory):
     meme_id = meme_factory()
     assert meme_id is not None
+
+
+@pytest.mark.positive
+def test_update_meme(start_end_testing, before_after_testing, meme_factory, update_meme_endpoint):
+    meme_id = meme_factory()
+    assert meme_id is not None
+
+    payload_to_send = upd_payload.copy()
+    payload_to_send['id'] = int(meme_id)
+
+    update_meme_endpoint.update_meme(payload=payload_to_send, meme_id=meme_id)
+    update_meme_endpoint.check_status_code_is_200()
 
 
 @pytest.mark.positive
