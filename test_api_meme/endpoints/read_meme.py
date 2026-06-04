bd1a5daed4c5386ef3c191eecfe77da24f185f70
AndrewShabailov <andrewshabailov@gmail.com>
2026-06-04 11:59:39 +0300

    code refactoring, method renaming

--- test_api_meme/endpoints/read_meme.py
+++ test_api_meme/endpoints/read_meme.py
@@ -2,7 +2,6 @@ import requests
 import allure
 from endpoints.base_meme import BaseMeme
 
-
 class ReadMeme(BaseMeme):
 
     @allure.step('Send GET request for reading all memes')
@@ -12,10 +11,9 @@ class ReadMeme(BaseMeme):
         )
         return self.response
 
-    @allure.step('Send GET request for checking token is alive')
-    def read_token_is_alive(self):
-        auth_payload = {"name": "Andrew"}
-        requests.post(
-            f'{BaseMeme.base_url}/authorize', json=auth_payload
-        ).json()
+    @allure.step('Send GET request to validate token')
+    def check_token(self):
+        self.response = requests.get(
+            f'{self.base_url}/meme', headers=self.headers
+        )
         return self.response
