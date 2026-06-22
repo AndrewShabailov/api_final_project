80a28c13f6a4aab5e1f4aedd74e83f8c04883453
AndrewShabailov <andrewshabailov@gmail.com>
2026-06-22 16:10:13 +0300

    authorization moving

--- test_api_meme/endpoints/auth_meme.py
+++ test_api_meme/endpoints/auth_meme.py
@@ -1,13 +1,16 @@
-import requests
 import allure
+import requests
 from endpoints.base_meme import BaseMeme
 
 
 class AuthMeme(BaseMeme):
 
-    @allure.step('Send POST request for authorization')
-    def login(self, login_payload):
-        self.response = requests.post(
-            f'{self.base_url}/authorize', json=login_payload
-        )
+    @allure.step('Send POST request to authorize')
+    def login(self, payload):
+        self.response = requests.post(f'{self.base_url}/authorize', json=payload)
+        return self.response
+
+    @allure.step('Send GET request to validate token')
+    def check_token_alive(self, token):
+        self.response = requests.get(f'{self.base_url}/authorize/{token}')
         return self.response
